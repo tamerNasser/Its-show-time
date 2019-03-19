a07f49d78f687526d639360e854bb40688d342c3
karam1ashqar <karam.ashqar01@gmail.com>
2019-03-19 19:20:00 +0200

    styled the items, centered them and added buttons of deleting item and checkbox for completing

--- skeleton/dom.js
+++ skeleton/dom.js
@@ -3,51 +3,72 @@
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container');
-  var addTodoForm = document.getElementById('add-todo');
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
-  var state = [{
+  var state = [
+    {
       id: -3,
-      name: 'first1',
-      desc: 'first todo',
+      name: "first1",
+      desc: "first todo",
       done: false
     },
     {
       id: -2,
-      name: 'second2',
-      desc: 'second todo',
+      name: "second2",
+      desc: "second todo",
       done: false
     },
     {
       id: -1,
-      name: 'third3',
-      desc: 'third todo',
+      name: "third3",
+      desc: "third todo",
       done: false
-    },
+    }
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
   let createTodoNode = function(todo) {
-    let todoNode = document.createElement('li');
-    todoNode.className = 'todo-items';
+    let todoNode = document.createElement("li");
+    todoNode.className = "todo-items";
 
+    let todoNameNode = document.createElement("h3");
+    todoNameNode.className = "todo-name";
+    let todoDescNode = document.createElement("p");
+    todoDescNode.className = "todo-desc";
 
-    let todoNameNode = document.createElement('p');
-    todoNameNode.className = 'todo-name';
-    let todoDescNode = document.createElement('p');
-    todoDescNode.className = 'todo-desc';
-
-    var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    var deleteButtonNode = document.createElement("button");
+    deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
     todoNameNode.innerText = todo.name;
     todoDescNode.innerText = todo.desc;
 
-    todoNode.appendChild(todoNameNode);
-    todoNode.appendChild(todoDescNode);
-    todoNode.appendChild(deleteButtonNode);
+    let checkButton = document.createElement("input");
+    checkButton.type = "checkbox";
+    let labelCheckButton = document.createElement("label");
+    //checkBtn.appendChild(document.)
+
+    let container = document.createElement("div");
+    container.className = "contentContainer";
+
+    todoNode.appendChild(checkButton);
+    todoNode.appendChild(labelCheckButton);
+
+    todoNode.appendChild(container);
+    container.appendChild(todoNameNode);
+    container.appendChild(document.createElement("hr"));
+    container.appendChild(todoDescNode);
+    //  container.appendChild(deleteButtonNode);
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "deleteBtn";
+
+    let editButton = document.createElement("button");
+    editButton.className = "editBtn";
+
+    todoNode.appendChild(deleteButton);
+    todoNode.appendChild(editButton);
 
     // add markTodo button
 
@@ -58,19 +79,18 @@
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
+    addTodoForm.addEventListener("submit", function(event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
       // what does event.preventDefault do?
       // what is inside event.target?
       event.preventDefault();
-      var description = '?'; // event.target ....
+      var description = "?"; // event.target ....
       let todoObj = {};
-      todoObj.name = document.getElementById('inputName').value;
+      todoObj.name = document.getElementById("inputName").value;
       todoObj.desc = "testDesc";
       // hint: todoFunctions.addTodo
       var newState = todoFunctions.addTodo(state, todoObj); // ?? change this!
       update(newState);
-
     });
   }
 
@@ -82,7 +102,7 @@
 
   // you do not need to change this function
   var renderState = function(state) {
-    var todoListNode = document.createElement('ul');
+    var todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
