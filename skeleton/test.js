904b22527e044a249a8ccf7ae14915dcd8a50c23
Tamer Nasser <tamernasser@outlook.com>
2019-03-19 14:42:09 +0200

    Added test for cloning todos array.3

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,47 @@
-var test = require('tape');
-var logic = require('./logic');
+var test = require("tape");
+var logic = require("./logic");
 
-test('Example test', function(t) {
-  t.pass();
+test("Testing cloneArrayOfObjects return value", function(t) {
+  let actual = logic.cloneArrayOfObjects([
+    {
+      id: 1,
+      name: "eat lunch",
+      desc: "don't forget to eat salaaadd",
+      done: false
+    },
+    {
+      id: 2,
+      name: "eat lunch2",
+      desc: "don't forget to eat salaaadd2",
+      done: true
+    },
+    {
+      id: 3,
+      name: "eat lunch3",
+      desc: "don't forget to eat salaaadd3",
+      done: false
+    }
+  ]);
+  var expected = [
+    {
+      id: 1,
+      name: "eat lunch",
+      desc: "don't forget to eat salaaadd",
+      done: false
+    },
+    {
+      id: 2,
+      name: "eat lunch2",
+      desc: "don't forget to eat salaaadd2",
+      done: true
+    },
+    {
+      id: 3,
+      name: "eat lunch3",
+      desc: "don't forget to eat salaaadd3",
+      done: false
+    }
+  ];
+  t.deepEqual(actual, expected, "Should return the same array of objects");
   t.end();
 });
