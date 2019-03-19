ba3aad25c11ebae769032b8ab63ae7fec92dca49
Tamer Nasser <tamernasser@outlook.com>
2019-03-19 14:51:48 +0200

    Added test for addingtodo

--- skeleton/test.js
+++ skeleton/test.js
@@ -2,8 +2,7 @@ var test = require("tape");
 var logic = require("./logic");
 
 test("Testing cloneArrayOfObjects return value", function(t) {
-  let actual = logic.cloneArrayOfObjects([
-    {
+  let actual = logic.cloneArrayOfObjects([{
       id: 1,
       name: "eat lunch",
       desc: "don't forget to eat salaaadd",
@@ -22,8 +21,7 @@ test("Testing cloneArrayOfObjects return value", function(t) {
       done: false
     }
   ]);
-  var expected = [
-    {
+  var expected = [{
       id: 1,
       name: "eat lunch",
       desc: "don't forget to eat salaaadd",
@@ -44,4 +42,63 @@ test("Testing cloneArrayOfObjects return value", function(t) {
   ];
   t.deepEqual(actual, expected, "Should return the same array of objects");
   t.end();
+});
+
+
+
+
+
+test("Testing - adding todo", function(t) {
+  var actual = logic.addTodo([{
+      id: 1,
+      name: "eat lunch",
+      desc: "don't forget to eat salaaadd",
+      done: false
+    },
+    {
+      id: 2,
+      name: "eat lunch2",
+      desc: "don't forget to eat salaaadd2",
+      done: true
+    },
+    {
+      id: 3,
+      name: "eat lunch3",
+      desc: "don't forget to eat salaaadd3",
+      done: false
+    }
+  ],
+   {
+    id: 4,
+    name: "don't eat lunch",
+    desc: " forget to eat salaaadd",
+    done: false
+  });
+  var expected = [{
+      id: 1,
+      name: "eat lunch",
+      desc: "don't forget to eat salaaadd",
+      done: false
+    },
+    {
+      id: 2,
+      name: "eat lunch2",
+      desc: "don't forget to eat salaaadd2",
+      done: true
+    },
+    {
+      id: 3,
+      name: "eat lunch3",
+      desc: "don't forget to eat salaaadd3",
+      done: false
+    },
+    {
+      id: 4,
+      name: "don't eat lunch",
+      desc: " forget to eat salaaadd",
+      done: false
+    }
+  ];
+ t.deepEqual(actual, expected, "Should return the todos array with the new todo object");
+ t.end();
 });
