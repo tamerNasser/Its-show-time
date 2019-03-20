a7492fe74c981e738f1897a6d270ac22c37d6e4c
karam1ashqar <karam.ashqar01@gmail.com>
2019-03-20 12:32:55 +0200

    added tests for sorting the object

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,6 +1,7 @@
 var test = require("tape");
 var logic = require("./logic");
-let todos = [{
+let todos = [
+  {
     id: 1,
     name: "eat lunch",
     desc: "don't forget to eat salaaadd",
@@ -20,7 +21,8 @@ let todos = [{
   }
 ];
 
-let todosMark = [{
+let todosMark = [
+  {
     id: 1,
     name: "eat lunch",
     desc: "don't forget to eat salaaadd",
@@ -47,48 +49,10 @@ test("Testing cloneArrayOfObjects return value", function(t) {
   t.end();
 });
 
-
-
-
-
-test("Testing - adding todo", function(t) {
-  var actual = logic.addTodo(todos, {
-    name: "don't eat lunch",
-    desc: " forget to eat salaaadd"
-  });
-  var expected = todos.concat(
+test("sorted by not done", function(t) {
+  let actual = logic.sortTodos(todos, "notDone");
+  var expected = [
     {
-      id: 1,
-      name: "don't eat lunch",
-      desc: " forget to eat salaaadd",
-      done: false
-    }
-  );
-  t.deepEqual(actual, expected, "Should return the todos array with the new todo object");
-  t.end();
-});
-
-test('Testing - deleting todo', function(t) {
-  var actual = logic.deleteTodo([{
-      id: 1,
-      name: "eat lunch",
-      desc: "don't forget to eat salaaadd",
-      done: false
-    },
-    {
-      id: 2,
-      name: "eat lunch2",
-      desc: "don't forget to eat salaaadd2",
-      done: true
-    },
-    {
-      id: 3,
-      name: "eat lunch3",
-      desc: "don't forget to eat salaaadd3",
-      done: false
-    }
-  ], 2);
-  var expected = [{
       id: 1,
       name: "eat lunch",
       desc: "don't forget to eat salaaadd",
@@ -101,14 +65,99 @@ test('Testing - deleting todo', function(t) {
       done: false
     }
   ];
-  t.deepEqual(actual, expected, 'Should return the arra without the deleted todo');
+  t.deepEqual(actual, expected, "Should return only notDone todos");
   t.end();
 });
 
+test("sorted by done", function(t) {
+  let actual = logic.sortTodos(todos);
+  var expected = todos;
+  t.deepEqual(actual, expected, "Should return recent todos");
+  t.end();
+});
 
-test('Testing markTodo return value', function(t) {
-  var actual = logic.markTodo(todos,2);
+test("sorted by done", function(t) {
+  let actual = logic.sortTodos(todos, "done");
+  var expected = [
+    {
+      id: 2,
+      name: "eat lunch2",
+      desc: "don't forget to eat salaaadd2",
+      done: true
+    }
+  ];
+  t.deepEqual(actual, expected, "Should return only done todos");
+  t.end();
+});
+
+test("Testing - adding todo", function(t) {
+  var actual = logic.addTodo(todos, {
+    name: "don't eat lunch",
+    desc: " forget to eat salaaadd"
+  });
+  var expected = todos.concat({
+    id: 1,
+    name: "don't eat lunch",
+    desc: " forget to eat salaaadd",
+    done: false
+  });
+  t.deepEqual(
+    actual,
+    expected,
+    "Should return the todos array with the new todo object"
+  );
+  t.end();
+});
+
+test("Testing - deleting todo", function(t) {
+  var actual = logic.deleteTodo(
+    [
+      {
+        id: 1,
+        name: "eat lunch",
+        desc: "don't forget to eat salaaadd",
+        done: false
+      },
+      {
+        id: 2,
+        name: "eat lunch2",
+        desc: "don't forget to eat salaaadd2",
+        done: true
+      },
+      {
+        id: 3,
+        name: "eat lunch3",
+        desc: "don't forget to eat salaaadd3",
+        done: false
+      }
+    ],
+    2
+  );
+  var expected = [
+    {
+      id: 1,
+      name: "eat lunch",
+      desc: "don't forget to eat salaaadd",
+      done: false
+    },
+    {
+      id: 3,
+      name: "eat lunch3",
+      desc: "don't forget to eat salaaadd3",
+      done: false
+    }
+  ];
+  t.deepEqual(
+    actual,
+    expected,
+    "Should return the arra without the deleted todo"
+  );
+  t.end();
+});
+
+test("Testing markTodo return value", function(t) {
+  var actual = logic.markTodo(todos, 2);
   var expected = todosMark;
-  t.deepEqual(actual, expected, 'Should return todos with the done toggled');
+  t.deepEqual(actual, expected, "Should return todos with the done toggled");
   t.end();
 });
